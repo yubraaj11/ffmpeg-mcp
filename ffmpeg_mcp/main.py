c350e73910c5a53ea717575287570adf05b6f348
yubraaj11 <yubraj@veelapp.com>
2025-09-17 11:10:08 +0545

    Add more functions as tools for mcp

--- ffmpeg_mcp/main.py
+++ ffmpeg_mcp/main.py
@@ -3,7 +3,16 @@ import logging
 from fastmcp import FastMCP
 
 from ffmpeg_mcp.configs import setup_logging
-from ffmpeg_mcp.services import extract_frames, extract_audio, clip_video, crop_video
+from ffmpeg_mcp.services import (
+	clip_video,
+	crop_video,
+	extract_audio,
+	extract_frames,
+	overlay_image,
+	overlays_video,
+	scale_video,
+	trim_and_concat_operation,
+)
 
 setup_logging()
 logger = logging.getLogger(__name__)
@@ -13,6 +22,10 @@ mcp.tool(name_or_fn=extract_frames)
 mcp.tool(name_or_fn=extract_audio)
 mcp.tool(name_or_fn=clip_video)
 mcp.tool(name_or_fn=crop_video)
+mcp.tool(name_or_fn=overlay_image)
+mcp.tool(name_or_fn=overlays_video)
+# mcp.tool(name_or_fn=trim_and_concat_operation)
+mcp.tool(name_or_fn=scale_video)
 
 
 def main():
