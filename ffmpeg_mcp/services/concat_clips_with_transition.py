2e7097b04cd8983271b88436403ba1bb9c9df1e7
yogeshpanta <yogeshpanta33@gmail.com>
2025-09-17 10:55:21 +0545

    feat:concat_transition update code to handle more than two clips

--- ffmpeg_mcp/services/concat_clips_with_transition.py
+++ ffmpeg_mcp/services/concat_clips_with_transition.py
@@ -5,20 +5,21 @@ from typing import List
 import ffmpeg
 
 from ffmpeg_mcp.configs import setup_logging
+from ffmpeg_mcp.exceptions import build_exception_message
 from ffmpeg_mcp.services.normalize_video_clips import get_normalized_clips
 from utils import calculate_video_offset, get_video_duration
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
-output_video_path = os.path.join(base_dir, '..', 'processed_elements', 'concatenated_video', 'final_edited_video.mp4')
+# output_video_path = os.path.join(base_dir, '..', 'processed_elements', 'concatenated_video', 'final_edited_video.mp4')
 video_clip_path = os.path.join(base_dir, '..', 'processed_elements', 'normalized_video_clips')
 
-os.makedirs(os.path.dirname(output_video_path), exist_ok=True)
+# os.makedirs(os.path.dirname(output_video_path), exist_ok=True)
 
 setup_logging()
 logger = logging.getLogger(__name__)
 
 
-def concat_clips_with_transition(input_video_clips: List[str], transition_type: str = 'fade', transition_duration: float = 2) -> str:
+def concat_clips_with_transition(input_video_clips: List[str], transition_type: str = 'fade', transition_duration: float = 0.5) -> str:
 	"""
 	Concatenate multiple video clips with transitions.
 
@@ -49,6 +50,9 @@ def concat_clips_with_transition(input_video_clips: List[str], transition_type: 
 	        The absolute path to the final concatenated video with transitions applied.
 	"""
 
+	output_video_path = os.path.join(base_dir, '..', 'processed_elements', 'concatenated_video', 'final_edited_video.mp4')
+	os.makedirs(os.path.dirname(output_video_path), exist_ok=True)
+
 	if len(input_video_clips) < 2:
 		logger.error('Need at lest two video clips to concat')
 		return None
@@ -56,7 +60,6 @@ def concat_clips_with_transition(input_video_clips: List[str], transition_type: 
 	get_normalized_clips(input_video_clips=input_video_clips)
 
 	video_list = sorted([os.path.join(video_clip_path, f) for f in os.listdir(video_clip_path) if f.endswith('.mp4')])
-
 	for i in range(1, len(video_list)):
 		clip_1 = video_list[0] if i == 1 else output_video_path
 		clip_2 = video_list[i]
@@ -76,19 +79,19 @@ def concat_clips_with_transition(input_video_clips: List[str], transition_type: 
 
 		offset_duration = calculate_video_offset(input_video_path=clip_1, transition_duration=transition_duration)
 		logger.info(f'offeset duration for clips is {offset_duration}')
-
-		v = ffmpeg.filter([video1, video2], 'xfade', transition=transition_type, duration=transition_duration, offset=offset_duration)
-		a = ffmpeg.filter([audio1, audio2], 'acrossfade', duration=transition_duration)
-
-		(ffmpeg.output(v, a, output_video_path, safe=0).run(overwrite_output=True))
-	# final_output = os.path.abspath(
-	# 	os.path.join(os.path.dirname(__file__), '..', 'processed_elements', 'concatenated_video', 'output_restructured_video.mp4')
-	# )
-	final_output = output_video_path
+		try:
+			v = ffmpeg.filter([video1, video2], 'xfade', transition=transition_type, duration=transition_duration, offset=offset_duration)
+			a = ffmpeg.filter([audio1, audio2], 'acrossfade', duration=transition_duration)
+			temp_output = output_video_path.replace('.mp4', f'_step_{i}.mp4')
+			(ffmpeg.output(v, a, temp_output, safe=0).run(overwrite_output=True, quiet=True))
+			output_video_path = temp_output
+		except ffmpeg.Error as e:
+			logger.error(f'Error occured in ffmpeg: {str(e)}')
+			build_exception_message(error_type=ffmpeg.Error, message=f'Error occured while concatenating video: {str(e)}')
+	final_output = os.path.abspath(
+		os.path.join(os.path.dirname(__file__), '..', 'processed_elements', 'concatenated_video', 'output_restructured_video.mp4')
+	)
 	if os.path.exists(output_video_path):
 		os.rename(output_video_path, final_output)
 		logger.info(f'final output saved to {final_output}')
 	return final_output
-
-if __name__ == "__main__":
-	print(concat_clips_with_transition(input_video_clips=["/home/yogesh/Downloads/vid2.MOV", "/home/yogesh/Downloads/vid3.MOV"]))
