4b856beee4a21f929f8f624980a709d8c6c581cd
yubraaj11 <yubraj@veelapp.com>
2025-09-18 14:51:02 +0545

    Add function in init file

--- ffmpeg_mcp/services/__init__.py
+++ ffmpeg_mcp/services/__init__.py
@@ -5,5 +5,6 @@ from ffmpeg_mcp.services.extract_frames import extract_frames
 from ffmpeg_mcp.services.get_video_metadata import get_video_metadata
 from ffmpeg_mcp.services.overlays_video import overlays_video
 from ffmpeg_mcp.services.trim_and_concatenate_video import trim_and_concat_operation	
+from ffmpeg_mcp.services.make_gif import make_gif
 
-__all__ = ['clip_video', 'crop_video', 'extract_audio', 'extract_frames', 'get_video_metadata', 'overlays_video', 'trim_and_concat_operation']
+__all__ = ['clip_video', 'crop_video', 'extract_audio', 'extract_frames', 'get_video_metadata', 'make_gif', 'overlays_video', 'trim_and_concat_operation']
