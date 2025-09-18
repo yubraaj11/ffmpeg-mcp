c2fd7df0a240cc7d7203d56b09cd746b41630328
yogeshpanta <yogeshpanta33@gmail.com>
2025-09-18 10:45:35 +0545

    feat:concat_transition remove video duration file from init

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,4 @@
 from utils.validate_input_video_path import validate_input_video_path
-from utils.get_video_duration import get_video_duration
 from utils.calculate_video_offset import calculate_video_offset
 
-__all__ = ['validate_input_video_path', 'get_video_duration', 'calculate_video_offset']
+__all__ = ['validate_input_video_path', 'calculate_video_offset']
